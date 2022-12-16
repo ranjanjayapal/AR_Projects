a3b2aeec4c5bc76cda99ba8adeab5d4d167b61e8
ranjanjayapal <rjayapal@stevens.edu>
2022-12-16 21:37:23 +0530

    AR cube on plane project

---
+++ AR_CubeonPlane/Assets/Scripts/PlaceOnPlane.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceOnPlane : MonoBehaviour
+{
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    public GameObject placedPrefab;
+    public GameObject spawnedObject { get; private set; }
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = s_Hits[0].pose;
+
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+            }
+        }
+    }
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+    ARRaycastManager m_RaycastManager;
+}
